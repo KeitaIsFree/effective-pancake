89b0f1d8af0a652d772a1862ed7e78709b865a1a
KeitaIsFree <maxdxd10108102@gmail.com>
2020-07-30 10:55:12 +0900

    Added line detection after sobel

--- convolution.py
+++ convolution.py
@@ -15,6 +15,8 @@ size = 'size'
 edge = 'edge'
 only_brightness = 'only_brightness'
 
+ANGLE_DIFF_THRESHOLD = math.pi / 8
+
 SOBEL_KERNEL = {
     'values': [
         [ [ 1, 0, -1 ],
@@ -138,6 +140,92 @@ def apply_filter(kernel = selected_kernel, data_list = img_list):
                     result[y][x] = (total[0], total[1], total[2])
     return result
 
+def label():
+    global result, img_list
+    lbl_list_1 = [ [0 for _ in range(len(img_list[0]))] for _ in range(len(img_list[1])) ]
+    lbl_img1 = Image.new('L', (len(img_list[0]), len(img_list)))
+    max_lbl = 0
+    used_lbls = []
+    clusters = {}
+    for y in range(len(img_list)):
+        print('Searching at y: '+str(y))
+        for x in range(len(img_list[0])):
+            if result[y][x]["value"] < 250:
+                continue
+            #pixels to compare to
+            cmp_pixels = []
+            if y > 0:
+                cmp_pixels.append((x,y-1))
+            if x > 0:
+                cmp_pixels.append((x-1,y))
+            continued_pixels = [] #pixels that are the similar color
+            continued_lbl_values = [] #label values of those
+            for cmp_pixel in cmp_pixels:
+                if -ANGLE_DIFF_THRESHOLD < result[cmp_pixel[1]][cmp_pixel[0]]["angle"] - result[y][x]["angle"] < ANGLE_DIFF_THRESHOLD:
+                    continued_pixels.append((cmp_pixel))
+                    continued_lbl_values.append(lbl_list_1[cmp_pixel[1]][cmp_pixel[0]])
+            #if for each numer of nearby pixels that are similar
+            if len(continued_pixels)==0:
+                lbl_list_1[y][x] = 0
+            elif len(continued_pixels)==1:
+                if continued_lbl_values[0]==0:
+                    max_lbl += 1
+                    lbl_list_1[continued_pixels[0][1]][continued_pixels[0][0]] = max_lbl
+                    lbl_list_1[y][x] = max_lbl
+                    used_lbls.append(max_lbl)
+                    clusters[str(max_lbl)] = 1
+                else:
+                    lbl_list_1[y][x] = continued_lbl_values[0]
+                    clusters[str(continued_lbl_values[0])] += 1
+            elif len(continued_pixels)==2:
+                if continued_lbl_values[0] == continued_lbl_values[1] and not continued_lbl_values[0] == 0:
+                    lbl_list_1[y][x] = continued_lbl_values[0]
+                    clusters[str(continued_lbl_values[0])] += 1
+                elif continued_lbl_values[0] == 0 and continued_lbl_values[1] == 0:
+                    max_lbl += 1
+                    lbl_list_1[continued_pixels[0][1]][continued_pixels[0][0]] = max_lbl
+                    lbl_list_1[continued_pixels[1][1]][continued_pixels[1][0]] = max_lbl
+                    lbl_list_1[y][x] = max_lbl
+                    clusters[str(max_lbl)] = 3
+                    used_lbls.append(max_lbl)
+                elif continued_lbl_values[0] == 0 or continued_lbl_values[1] == 0:
+                    label = continued_lbl_values[0] + continued_lbl_values[1]
+                    lbl_list_1[continued_pixels[0][1]][continued_pixels[0][0]] = label
+                    lbl_list_1[continued_pixels[1][1]][continued_pixels[1][0]] = label
+                    lbl_list_1[y][x] = label
+                    clusters[str(label)] += 2
+                else :
+                    lbl_list_1[y][x] = lbl_list_1[continued_pixels[0][1]][continued_pixels[0][0]]
+                    #All pixels with the same label as (x-1,y) must be changed to the same label as (x,y-1)
+                    change_from = lbl_list_1[continued_pixels[1][1]][continued_pixels[1][0]]
+                    used_lbls.remove(change_from)
+                    change_to = lbl_list_1[continued_pixels[0][1]][continued_pixels[0][0]]
+                    clusters[str(change_to)] += (clusters[str(change_from)] + 1)
+                    clusters.pop(str(change_from))
+                    for s in range(y+1):
+                        for t in range(len(img_list[0])):
+                            if lbl_list_1[s][t]==change_from:
+                                lbl_list_1[s][t] = change_to
+    to_be_removed = []
+    for label in used_lbls:
+        if clusters[str(label)] < 256:
+            clusters.pop(str(label))
+            to_be_removed.append(label)
+    for label in to_be_removed:
+        used_lbls.remove(label)
+    for y in range(len(img_list[1])):
+        for x in range(len(img_list[0])):
+            if lbl_list_1[y][x] in to_be_removed:
+                lbl_list_1[y][x] = 0
+            elif lbl_list_1[y][x] != 0:
+                lbl_list_1[y][x] = used_lbls.index(lbl_list_1[y][x])
+    for y in range(len(img_list[1])):
+        for x in range(len(img_list[0])):
+            if not lbl_list_1[y][x]==0:
+                lbl_img1.putpixel((x,y), 255)
+    print('Number of clusters :'+str(len(used_lbls)))
+    lbl_img1.show()
+
 def show():
     global result_img
     result_img.show()
@@ -145,6 +233,7 @@ def show():
 def main():
     load_image()
     apply_filter()
+    label()
     show()
 
 if __name__=='__main__':
